8d682f85fc2fe27f40ee022967547e85ba318acf
Alexandra Tsampikakis <alexandra.tsampikakis@gmail.com>
2013-06-20 13:33:04 +0200

    First commit!

---
+++ gen/com/raywenderlich/Zombie/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package com.raywenderlich.Zombie;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
